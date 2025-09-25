2ec41430e99fe3d8090080abf914c82b06b12914
RiaVirk <virkmariaofficial@gmail.com>
2025-09-25 09:36:47 +0200

    1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: { unoptimized: true }, // Required for static export
+};
 
 export default nextConfig;
